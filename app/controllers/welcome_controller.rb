23a2b6382822e96a78a04f7040b61f28891daed6
technologyRiff <technologyriff@gmail.com>
2015-01-16 14:14:46 -0500

    commit before meeting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+  include Pundit
 
-  def about
-  end
+  protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+      rescue_from Pundit::NotAuthorizedError do |exception|
+        redirect_to root_url, alert: exception.message
+      end
+    
 end
